2c469dd6df2a855f8925004816b65f85793edc00
priyanshutariyal02 <138609182+priyanshutariyal02@users.noreply.github.com>
2023-07-18 19:01:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+
+function startPlaying() {
+    var randomNumber1 = Math.floor((Math.random() * 6) + 1);
+
+    var randomDiceImg = "dice" + randomNumber1 + ".png";// dice1.png-dice2.png
+
+    var randomImgSrc1 = "images/" + randomDiceImg;// images/dice1.png - dice2.png
+
+    var image1 = document.querySelectorAll("img")[0];
+
+    image1.setAttribute("src", randomImgSrc1);
+
+    
+    var randomNumber2 = Math.floor((Math.random() * 6) + 1);
+
+    var randomImgSrc2 = "images/dice" + randomNumber2 + ".png"
+
+    document.querySelectorAll("img")[1].setAttribute("src", randomImgSrc2);
+
+    if (randomNumber1 > randomNumber2) {
+        document.querySelector("h1").innerHTML = "🚩Player 1 Wins!"
+    }
+    else if (randomNumber1 < randomNumber2) {
+        document.querySelector("h1").innerHTML = "🚩Player 2 Wins!";
+    }
+    else {
+        document.querySelector("h1").innerHTML = "Draw!";
+    }
+}
